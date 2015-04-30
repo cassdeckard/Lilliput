bb8565d863151e1ea613af172050f63052dafba8
Asynchrony Dev <dev1@charter-md2.asynchrony.com>
2015-04-30 10:47:39 -0500

    Test that we can inject the mock

--- LilliputTests/LilliputTests.swift
+++ LilliputTests/LilliputTests.swift
@@ -2,6 +2,18 @@ import Cocoa
 import XCTest
 
 class LilliputTests: XCTestCase {
+
+    class TestClass {
+        typealias StringFilter = (String) -> (String)
+        let stringFilter : StringFilter
+
+        init(stringFilter : StringFilter) {
+            self.stringFilter = stringFilter
+        }
+
+        func useStringFilter(inString: String) -> String {
+            return self.stringFilter(inString)
+        }
     }
 
     func testBasicFunctionMocking() {
@@ -16,6 +28,26 @@ class LilliputTests: XCTestCase {
 
         let passedArg = "argument"
         let result = (*mockStringFilter)(passedArg)
+
+        verifyAtLeastOnce(mockStringFilter)
+        XCTAssertEqual(capturedArg!, passedArg)
+        XCTAssertEqual(result, expectedResult)
+    }
+
+    func testThatWeCanInjectTheMock() {
+        let mockStringFilter = MockFunc<String, String>()
+        var capturedArg : String?
+        let expectedResult = "aResult"
+        when(mockStringFilter) {
+            (arg: String) in
+            capturedArg = arg
+            return expectedResult
+        }
+
+        let passedArg = "anArgument"
+        let testClass = TestClass(stringFilter: *mockStringFilter)
+
+        let result = testClass.useStringFilter(passedArg)
 
         verifyAtLeastOnce(mockStringFilter)
         XCTAssertEqual(capturedArg!, passedArg)
