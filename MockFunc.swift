b158cb6bb89f2daf52a777c1f0336dd59adc2123
Asynchrony Dev <dev1@charter-md2.asynchrony.com>
2015-04-30 09:19:21 -0500

    Add verifyAtLeastOnce to MockFunc

--- MockFunc.swift
+++ MockFunc.swift
@@ -1,4 +1,5 @@
 import Foundation
+import XCTest
 
 class MockFunc<Input, Output> {
     typealias MockedFunc = (Input) -> (Output)
@@ -24,4 +25,14 @@ func when<Input, Output>(mockFunc : MockFunc<Input, Output>, callback: MockFunc<
 prefix operator * {}
 prefix func *<Input, Output>(mockFunc: MockFunc<Input, Output>) -> (MockFunc<Input, Output>.MockedFunc) {
     return mockFunc.call
+}
+
+extension XCTestCase {
+    func verifyAtLeastOnce<Input, Output>(mockFunc: MockFunc<Input, Output>,
+        inFile filePath: String = __FILE__,
+        atLine lineNumber: UInt = __LINE__) -> () {
+            if (mockFunc.calls.count < 1) {
+                self.recordFailureWithDescription("Mocked function was not called at least once", inFile: filePath, atLine: lineNumber, expected: true)
+            }
+    }
 }
