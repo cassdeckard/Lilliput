5e8cdbdd20b3cd15bdb5f018f0c77b716a3811f5
Asynchrony Dev <dev1@charter-md2.asynchrony.com>
2015-04-30 10:35:41 -0500

    Add MockFunc to project, with test

--- LilliputTests/LilliputTests.swift
+++ LilliputTests/LilliputTests.swift
@@ -1,36 +1,24 @@
-//
-//  LilliputTests.swift
-//  LilliputTests
-//
-//  Created by dev1 on 4/30/15.
-//  Copyright (c) 2015 Asynchrony. All rights reserved.
-//
-
 import Cocoa
 import XCTest
 
 class LilliputTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+
+    func testBasicFunctionMocking() {
+        let mockStringFilter = MockFunc<String, String>()
+        var capturedArg : String?
+        let expectedResult = "result"
+        when(mockStringFilter) {
+            (arg: String) in
+            capturedArg = arg
+            return expectedResult
         }
+
+        let passedArg = "argument"
+        let result = (*mockStringFilter)(passedArg)
+
+        verifyAtLeastOnce(mockStringFilter)
+        XCTAssertEqual(capturedArg!, passedArg)
+        XCTAssertEqual(result, expectedResult)
     }
-    
 }
