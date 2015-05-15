eb18d3aefee4f8c6602fc3d8e7b86cdbac3daf91
Matt Deckard <matthew.d.deckard@gmail.com>
2015-05-15 13:50:31 -0500

    Start project over with different design ideas

---
+++ Lilliput/Lilliput.h
@@ -0,0 +1,19 @@
+//
+//  Lilliput.h
+//  Lilliput
+//
+//  Created by dev1 on 4/30/15.
+//  Copyright (c) 2015 Asynchrony. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Lilliput.
+FOUNDATION_EXPORT double LilliputVersionNumber;
+
+//! Project version string for Lilliput.
+FOUNDATION_EXPORT const unsigned char LilliputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lilliput/PublicHeader.h>
+
+
