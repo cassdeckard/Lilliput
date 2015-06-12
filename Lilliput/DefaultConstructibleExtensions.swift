1476e9c0a9e20490666eb8eda4d0f4354a3fe11e
Matt Deckard <matthew.d.deckard@gmail.com>
2015-06-12 09:44:03 -0500

    Add a bunch of DefaultConstructible extensions
    Closes #8

--- Lilliput/DefaultConstructibleExtensions.swift
+++ Lilliput/DefaultConstructibleExtensions.swift
@@ -1,3 +1,31 @@
 import Foundation
 
-extension String: DefaultConstructible {}
+extension Array : DefaultConstructible {}
+extension AutoreleasingUnsafeMutablePointer : DefaultConstructible {}
+extension Bool : DefaultConstructible {}
+extension COpaquePointer : DefaultConstructible {}
+extension ContiguousArray : DefaultConstructible {}
+extension Dictionary : DefaultConstructible {}
+extension Double : DefaultConstructible {}
+extension EmptyCollection : DefaultConstructible {}
+extension EmptyGenerator : DefaultConstructible {}
+extension Float : DefaultConstructible {}
+extension Float80 : DefaultConstructible {}
+extension Int : DefaultConstructible {}
+extension Int16 : DefaultConstructible {}
+extension Int32 : DefaultConstructible {}
+extension Int64 : DefaultConstructible {}
+extension Int8 : DefaultConstructible {}
+extension StaticString : DefaultConstructible {}
+extension String : DefaultConstructible {}
+extension UInt : DefaultConstructible {}
+extension UInt16 : DefaultConstructible {}
+extension UInt32 : DefaultConstructible {}
+extension UInt64 : DefaultConstructible {}
+extension UInt8 : DefaultConstructible {}
+extension UTF16 : DefaultConstructible {}
+extension UTF32 : DefaultConstructible {}
+extension UTF8 : DefaultConstructible {}
+extension UnicodeScalar : DefaultConstructible {}
+extension UnsafeMutablePointer : DefaultConstructible {}
+extension UnsafePointer : DefaultConstructible {}
