b6b4df50ca61fb4090339f59af4dbe3c6e991a79
Matt Deckard <matthew.d.deckard@gmail.com>
2015-06-12 07:32:02 -0500

    Move MockWithBinding to seperate file

--- Lilliput/Lilliput.swift
+++ Lilliput/Lilliput.swift
@@ -80,35 +80,6 @@ func unbox<A: Equatable, ReturnType>(mock: MockFunction<A, NoArgument, ReturnTyp
     return { _unbox(mock, argA: $0, argB: NoArgument()) }
 }
 
-// MARK: Bindings
-
-class MockWithBinding<A: Equatable, B: Equatable, ReturnType> {
-    typealias BindingT = Binding<A, B>
-    var mock: Mock
-    var binding: BindingT
-
-    init(mock: Mock, binding: BindingT) {
-        self.binding = binding
-        self.mock = mock
-    }
-
-    func then<ReturnType>(returnValue: ReturnType) -> MockFunctionWithoutDefaultReturn<A, B, ReturnType> {
-        if let mock = self.mock as? MockFunctionWithoutDefaultReturn<A, B, ReturnType> {
-            mock.addBinding(binding: binding, returnValue: returnValue)
-            return mock
-        }
-        return MockFunctionWithoutDefaultReturn<A, B, ReturnType>(testCase: binding.testCase, bindings: [(binding, returnValue)])
-    }
-
-    func then<ReturnType>(returnValue: ReturnType) -> MockFunctionUsingDefaultConstructorForReturn<A, B, ReturnType> {
-        if let mock = self.mock as? MockFunctionUsingDefaultConstructorForReturn<A, B, ReturnType> {
-            mock.addBinding(binding: binding, returnValue: returnValue)
-            return mock
-        }
-        return MockFunctionUsingDefaultConstructorForReturn<A, B, ReturnType>(testCase: binding.testCase, bindings: [(binding, returnValue)])
-    }
-}
-
 // MARK: Syntactic Sugar
 
 class MockBuilder<A: Equatable, B: Equatable> {
