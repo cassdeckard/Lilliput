93451e22804550b8fcd5590bfd5214fc8abfba76
dev1 <dev@spr-md4.schafer.lan>
2016-09-27 11:00:47 -0500

    Enforce matching

--- Lilliput/Binding.swift
+++ Lilliput/Binding.swift
@@ -1,24 +1,55 @@
 import Foundation
 import XCTest
 
-class MockFunction<A, R> {
+class MockFunction<A: Equatable, R> {
+    let binding: Binding<A>
     let returnValue: R
+    let defaultValue: R
     
-    init(binding: Binding<A>, to value: R) {
-        returnValue = value
+    init(binding: Binding<A>, to returnValue: R, withDefault defaultValue: R) {
+        self.binding = binding
+        self.returnValue = returnValue
+        self.defaultValue = defaultValue
     }
     
     func unbox() -> (A) -> (R) {
-        return { _ in self.returnValue }
+        return {
+            if self.binding.matches($0) {
+                return self.returnValue
+            }
+            else {
+                return self.defaultValue
+            }
+        }
     }
 }
 
-class Binding<A>: NSObject {
-    init(testCase: XCTestCase, _ argA: A) {
-        
+class MockFunctionWithoutDefault<A: Equatable, R> {
+    let binding: Binding<A>
+    let returnValue: R
+    
+    init(binding: Binding<A>, to returnValue: R) {
+        self.binding = binding
+        self.returnValue = returnValue
     }
     
-    func then<R>(_ returnValue: R) -> MockFunction<A, R> {
-        return MockFunction(binding: self, to: returnValue)
+    func `else`(_ defaultValue: R) -> MockFunction<A, R> {
+        return MockFunction(binding: self.binding, to: returnValue, withDefault: defaultValue)
+    }
+}
+
+class Binding<A: Equatable>: NSObject {
+    let argA: A
+    
+    init(testCase: XCTestCase, _ argA: A) {
+        self.argA = argA
+    }
+    
+    func then<R>(_ returnValue: R) -> MockFunctionWithoutDefault<A, R> {
+        return MockFunctionWithoutDefault(binding: self, to: returnValue)
+    }
+    
+    func matches(_ argA: A) -> Bool {
+        return self.argA == argA
     }
 }
--- LilliputTests/LilliputTests.swift
+++ LilliputTests/LilliputTests.swift
@@ -3,10 +3,18 @@ import XCTest
 
 class LilliputTests: XCTestCase {
     func test_returnsExpectedValueWhenInputMatches() {
-        let mock_String👉String = when("foo").then("bar")
+        let mock_String👉String = when("foo").then("bar").else("baz")
         
         let callable: (String) -> (String) = mock_String👉String.unbox()
         
         XCTAssertEqual(callable("foo"), "bar")
+    }
+    
+    func test_returnsDefaultValueWhenInputDoesNotMatch() {
+        let mock_String👉String = when("foo").then("bar").else("baz")
+        
+        let callable: (String) -> (String) = mock_String👉String.unbox()
+        
+        XCTAssertEqual(callable("FOUX"), "baz")
     }
 }
