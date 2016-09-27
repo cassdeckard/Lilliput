aef857ba190d7591d96280f6d5c5f82e2699c9f7
dev1 <dev@spr-md4.schafer.lan>
2016-09-27 10:27:57 -0500

    New project

---
+++ Lilliput/Lilliput.h
@@ -0,0 +1,19 @@
+//
+//  Lilliput.h
+//  Lilliput
+//
+//  Created by dev1 on 9/27/16.
+//  Copyright © 2016 Matt Deckard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lilliput.
+FOUNDATION_EXPORT double LilliputVersionNumber;
+
+//! Project version string for Lilliput.
+FOUNDATION_EXPORT const unsigned char LilliputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lilliput/PublicHeader.h>
+
+
