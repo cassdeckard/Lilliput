de0fc43598a2f51cbf10cfc30b314fc60d705cb0
dev1 <dev1@bw-botf-md7.asynchrony.com>
2016-10-07 12:09:49 -0500

    Different kinds of matchers

--- Lilliput/MyPlayground.playground/Contents.swift
+++ Lilliput/MyPlayground.playground/Contents.swift
@@ -5,56 +5,67 @@ import UIKit
 var str = "Hello, playground"
 
 
-struct Mock<M: Matcher & Hashable, R> {
-    var bindings = [M : R]()
+struct Mock<M: Matcher, R> {
+    typealias Binding = (matcher: M, result: R)
+    var bindings = [Binding]()
 
     func match(_ a1: M.Arg1) -> R? {
         return bindings.filter {
-            $0.key.matches(a1)
+            $0.matcher.matches(a1)
         }.map {
-            $0.value
+            $0.result
         }.first
     }
 }
 
-protocol Matcher: Hashable {
+protocol Matcher {
     associatedtype Arg1
-
-    func then<R>(_ r: R) -> Mock<Self, R>
 
     func matches(_ a1: Arg1) -> Bool
 }
 
+extension Matcher {
+    internal func then<R>(_ r: R) -> Mock<Self, R> {
+        var mock = Mock<Self, R>()
+        mock.bindings.append((matcher: self, result: r))
+        return mock
+    }
+}
+
 //=========================================
 
-struct BoundArgumentMatcher<A1: Hashable> {
+struct BoundArgumentMatcher<A1: Equatable> {
     let a1: A1
-}
-
-extension BoundArgumentMatcher: Equatable {}
-func ==<E> (lhs: BoundArgumentMatcher<E>, rhs: BoundArgumentMatcher<E>) -> Bool {
-    return false
-}
-
-extension BoundArgumentMatcher: Hashable {
-    public var hashValue: Int { return a1.hashValue }
 }
 
 extension BoundArgumentMatcher: Matcher {
     typealias Arg1 = A1
-
-    internal func then<R>(_ r: R) -> Mock<BoundArgumentMatcher<A1>, R> {
-        var mock = Mock<BoundArgumentMatcher<A1>, R>()
-        mock.bindings[self] = r
-        return mock
-    }
 
     internal func matches(_ a1: A1) -> Bool {
         return self.a1 == a1
     }
 }
 
+
 //=========================================
+
+struct ClosureMatcher<A1> {
+    let closure: (A1) -> Bool
+}
+
+extension ClosureMatcher: Matcher {
+    typealias Arg1 = A1
+
+    internal func matches(_ a1: A1) -> Bool {
+        return closure(a1)
+    }
+}
+
+//=========================================
+
+func when<A1>(_ closure: @escaping (A1) -> Bool) -> ClosureMatcher<A1> {
+    return ClosureMatcher(closure: closure)
+}
 
 func when<A1>(_ a1: A1) -> BoundArgumentMatcher<A1> {
     return BoundArgumentMatcher(a1: a1)
@@ -66,3 +77,8 @@ var mock1 = when(2).then("bar")
 mock1.match(1)
 mock1.match(2)
 
+var mock2 = when{ $0 < 3 }.then("foo")
+mock2.match(1)
+mock2.match(2)
+mock2.match(3)
+mock2.match(4)
