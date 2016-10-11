08b63bf910dc058dd7b1c577fee3b6435412167b
dev1 <dev1@bw-botf-md7.asynchrony.com>
2016-10-11 14:05:55 -0500

    Add any() matcher

--- Lilliput/MyPlayground.playground/Contents.swift
+++ Lilliput/MyPlayground.playground/Contents.swift
@@ -13,8 +13,7 @@ protocol DefaultConstructible {
 extension String: DefaultConstructible {}
 
 class Mock<A1, R> {
-    typealias Matcher = AnyMatcher<A1>
-    typealias Binding = (matcher: AnyMatcher<A1>, result: R)
+    typealias Binding = (matcher: AnyMatcherType<A1>, result: R)
     var bindings = [Binding]()
     var defaultReturn: R?
 
@@ -52,12 +51,16 @@ extension Mock {
     func when(_ closure: @escaping ClosureMatcherWithTarget<A1, R>.Closure) -> ClosureMatcherWithTarget<A1, R> {
         return ClosureMatcherWithTarget(closure, target: self)
     }
+
+    func when<M: Matcher>(_ m: M) -> AnyMatcherTypeWithTarget<A1, R> where M.Arg1 == A1 {
+        return AnyMatcherTypeWithTarget(m, target: self)
+    }
 }
 
 //=========================================
 
-struct AnyMatcher<A1>: Matcher {
-    private let _match: AnyMatcher.Closure
+class AnyMatcherType<A1>: Matcher {
+    private let _match: AnyMatcherType.Closure
 
     typealias Arg1 = A1
 
@@ -69,6 +72,17 @@ struct AnyMatcher<A1>: Matcher {
 
     internal func matches(_ a1: A1) -> Bool {
         return _match(a1)
+    }
+}
+
+class AnyMatcherTypeWithTarget<A1, R>: AnyMatcherType<A1>, MatcherWithTarget {
+    internal var target: Mock<A1, R>
+
+    typealias Return = R
+
+    init<M: Matcher>(_ matcher: M, target: Mock<A1, R>) where M.Arg1 == Arg1 {
+        self.target = target
+        super.init(matcher)
     }
 }
 
@@ -88,7 +102,7 @@ extension Matcher {
 extension Matcher {
     internal func then<R>(_ r: R) -> Mock<Arg1, R> {
         let mock = Mock<Arg1, R>()
-        mock.bindings.append((matcher: AnyMatcher(self), result: r))
+        mock.bindings.append((matcher: AnyMatcherType(self), result: r))
         return mock
     }
 }
@@ -101,7 +115,7 @@ protocol MatcherWithTarget: Matcher {
 
 extension MatcherWithTarget {
     internal func then(_ r: Return) -> Mock<Arg1, Return> {
-        let binding = (matcher: AnyMatcher(self), result: r)
+        let binding = (matcher: AnyMatcherType(self), result: r)
         target.bindings.append(binding)
         return target
     }
@@ -164,12 +178,33 @@ class ClosureMatcherWithTarget<A1, R>: ClosureMatcher<A1>, MatcherWithTarget {
 
 //=========================================
 
+class AnyMatcher<A1> {
+}
+
+extension AnyMatcher: Matcher {
+    typealias Arg1 = A1
+
+    internal func matches(_ a1: A1) -> Bool {
+        return true
+    }
+}
+
+func any<T>(_ ttype: T.Type) -> AnyMatcher<T> {
+    return AnyMatcher<T>()
+}
+
+//=========================================
+
 func when<A1>(_ closure: @escaping ClosureMatcher<A1>.Closure) -> ClosureMatcher<A1> {
     return ClosureMatcher(closure)
 }
 
 func when<A1>(_ a1: A1) -> BoundArgumentMatcher<A1> {
     return BoundArgumentMatcher(a1)
+}
+
+func when<M: Matcher, A1>(_ m: M) -> AnyMatcherType<A1> where M.Arg1 == A1 {
+    return AnyMatcherType<A1>(m)
 }
 
 //=========================================
@@ -206,8 +241,16 @@ mock2Func(0)   // NOTE: when multiple matchers match, the first wins
 mock2Func(18)
 mock2Func(8)
 
+mock2.when(any(Int.self)).then("Any int is fine!")
+mock2Func(30930)
+
+var mock3 = when(any(Int.self)).then(3)
+var mock3Func = *mock3
+mock3Func(-1)
+mock3Func(65535)
+
+
 // TODO: verify times
 
 // TODO: multiple arguments
 
-// TODO: any matcher
